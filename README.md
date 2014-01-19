7e69b39dbf743d7f851f0f505e0b6e04a8a05f77
Jonah Kirangi <jonahkirangi@gmail.com>
2014-01-19 00:19:20 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Zurbify A New Rails Store
+
+A small web application generated
+using [Zurb](https://github.com/zurb/foundation)
+and following [RailsCast # 417](http://railscasts.com/episodes/417-foundation)
+as a guide.
+
+http://i.imgur.com/pFXraA1.png
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store2::Application.config.secret_key_base = '291118d79e0d5bd654e977edc9192f4b4fa03d8680b1e064709e60f5c71dd60781e1652c03aa0951e6bd5605f8bc9c05a7601275bf046cac373c480f4a7b4c52'
